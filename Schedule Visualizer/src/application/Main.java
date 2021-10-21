71cd3aba05032f41122494a05d7040cb04508ae7
nicholasgerasch18 <nicholasgerasch18@OlinLab204-04.augustana.edu>
2021-10-21 10:15:00 -0500

    adding calendar days and work on parsing string

--- Schedule Visualizer/src/application/Main.java
+++ Schedule Visualizer/src/application/Main.java
@@ -18,14 +18,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
+			final int COLUMN_NUM = 12;
 			
 			primaryStage.setTitle("Schedule Visualizer");
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			root.setPadding(new Insets(10));
 			
-			System.out.print("hi");
+			
 			//creating window elements
 			Text scheduleVisualizer = new Text("Augustana Schedule Visualizer");
 			root.setAlignment(scheduleVisualizer, Pos.TOP_CENTER);
@@ -49,16 +49,51 @@ public class Main extends Application {
 			     public void handle(ActionEvent e) {
 			         GridPane newRoot = new GridPane();
 			         
+			         String input = textArea.getText();
+			         String[] splitInput = input.split("\n");
+			         int size = (splitInput.length/COLUMN_NUM)+1;
+			         System.out.print(size);
+			         
+			         for(int i = 0; i<splitInput.length;i++) {
+			        	 System.out.println(splitInput[i]);
+			         }
+			         
+			         
+			         
+//			         String[][] classMatrix = new String[size][COLUMN_NUM];
+//			         int count = 0;
+//			         for(int i = 0; i<size;i++) {
+//			        	 for(int n = 0; i<COLUMN_NUM;n++) {
+//			        		 String current = splitInput[count];
+//			        		 classMatrix[i][n]="hi";
+//			        		 count++;
+//			        	 }
+//			         }
+//			         		         
+			         
+			         Text monday = new Text("		Monday		");		
+			         Text tuesday = new Text("Tuesday		");			         
+			         Text wednesday = new Text("Wednesday		");			         
+			         Text thursday = new Text("Thursday		");			         
+			         Text friday = new Text("Friday		");			         
+			         Text saturday = new Text("Saturday		");			         
+			         Text sunday = new Text("Sunday");			         
 
-			         Text formattedSchedule = new Text("Your schedule looks like this. Anas, Justin, Gabe, Nick");			         
-
-			         newRoot.add(formattedSchedule, 0, 0);
+			         newRoot.add(monday, 0, 0);
+			         newRoot.add(tuesday, 1, 0);
+			         newRoot.add(wednesday, 2, 0);
+			         newRoot.add(thursday, 3, 0);
+			         newRoot.add(friday, 4, 0);
+			         newRoot.add(saturday, 5, 0);
+			         newRoot.add(sunday, 6, 0);
+			         
+			         
 			         newRoot.setPadding(new Insets(10));
 			         newRoot.setVgap(15);
 			         newRoot.setHgap(15);
 			         Stage stage = new Stage();
 			         stage.setTitle("New Visualized Schedule!!!!!");
-			         stage.setScene(new Scene(newRoot,400,200));
+			         stage.setScene(new Scene(newRoot,750,200));
 			         stage.show();
 			     }
 			 });
