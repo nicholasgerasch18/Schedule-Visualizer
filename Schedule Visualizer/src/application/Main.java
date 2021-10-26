588f0424f0858b906307b0b15473f9f4c5205b4f
nicholasgerasch18 <nicholasgerasch18@OlinLab204-04.augustana.edu>
2021-10-26 12:01:22 -0500

    testing parsing

--- Schedule Visualizer/src/application/Main.java
+++ Schedule Visualizer/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,53 +13,66 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
-
 public class Main extends Application {
+	
+	final int COLUMN_NUM = 12;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			final int COLUMN_NUM = 12;
 			
+
 			primaryStage.setTitle("Schedule Visualizer");
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 400, 400);
 			root.setPadding(new Insets(10));
-			
-			
-			//creating window elements
+
+			// creating window elements
 			Text scheduleVisualizer = new Text("Augustana Schedule Visualizer");
 			root.setAlignment(scheduleVisualizer, Pos.TOP_CENTER);
-		    root.setTop(scheduleVisualizer);
-			
+			root.setTop(scheduleVisualizer);
+
 			TextArea textArea = new TextArea();
 			root.setMargin(textArea, new Insets(20));
-		    root.setCenter(textArea);
-			
-		    Button submit = new Button("Submit");
-		    root.setAlignment(submit, Pos.BOTTOM_CENTER);
-		    root.setMargin(submit, new Insets(10));
-		    root.setBottom(submit);
-		    
-		    
+			root.setCenter(textArea);
+
+			Button submit = new Button("Submit");
+			root.setAlignment(submit, Pos.BOTTOM_CENTER);
+			root.setMargin(submit, new Insets(10));
+			root.setBottom(submit);
+
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
+
 			submit.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
-			     public void handle(ActionEvent e) {
-			         GridPane newRoot = new GridPane();
-			         
-			         String input = textArea.getText();
-			         String[] splitInput = input.split("\n");
-			         int size = (splitInput.length/COLUMN_NUM)+1;
-			         System.out.print(size);
-			         
-			         for(int i = 0; i<splitInput.length;i++) {
-			        	 System.out.println(splitInput[i]);
-			         }
-			         
-			         
-			         
+				public void handle(ActionEvent e) {
+					GridPane newRoot = new GridPane();
+
+					String input = textArea.getText();
+					String[] splitInput = input.split("\n");
+					int length = splitInput.length;
+					int size = ((splitInput.length)/COLUMN_NUM);
+					System.out.println(size);
+					
+			
+					int count = COLUMN_NUM;
+					
+					
+					for (int j = count; j < length; j++) {
+						
+						
+						String[] courseInfo = new String[COLUMN_NUM];
+						for (int i = 0; i < COLUMN_NUM; i++) {
+							
+							count++;
+						}
+						Course course = new Course(courseInfo);
+					}
+					
+//					for (int i = 0; i < splitInput.length; i++) {
+//						System.out.println(splitInput[i]);
+//					}
+
 //			         String[][] classMatrix = new String[size][COLUMN_NUM];
 //			         int count = 0;
 //			         for(int i = 0; i<size;i++) {
@@ -70,39 +83,53 @@ public class Main extends Application {
 //			        	 }
 //			         }
 //			         		         
-			         
-			         Text monday = new Text("		Monday		");		
-			         Text tuesday = new Text("Tuesday		");			         
-			         Text wednesday = new Text("Wednesday		");			         
-			         Text thursday = new Text("Thursday		");			         
-			         Text friday = new Text("Friday		");			         
-			         Text saturday = new Text("Saturday		");			         
-			         Text sunday = new Text("Sunday");			         
 
-			         newRoot.add(monday, 0, 0);
-			         newRoot.add(tuesday, 1, 0);
-			         newRoot.add(wednesday, 2, 0);
-			         newRoot.add(thursday, 3, 0);
-			         newRoot.add(friday, 4, 0);
-			         newRoot.add(saturday, 5, 0);
-			         newRoot.add(sunday, 6, 0);
-			         
-			         
-			         newRoot.setPadding(new Insets(10));
-			         newRoot.setVgap(15);
-			         newRoot.setHgap(15);
-			         Stage stage = new Stage();
-			         stage.setTitle("New Visualized Schedule!!!!!");
-			         stage.setScene(new Scene(newRoot,750,200));
-			         stage.show();
-			     }
-			 });
-			
-		} catch(Exception e) {
+					Text monday = new Text("		Monday		");
+					Text tuesday = new Text("Tuesday		");
+					Text wednesday = new Text("Wednesday		");
+					Text thursday = new Text("Thursday		");
+					Text friday = new Text("Friday		");
+					Text saturday = new Text("Saturday		");
+					Text sunday = new Text("Sunday");
+
+					newRoot.add(monday, 0, 0);
+					newRoot.add(tuesday, 1, 0);
+					newRoot.add(wednesday, 2, 0);
+					newRoot.add(thursday, 3, 0);
+					newRoot.add(friday, 4, 0);
+					newRoot.add(saturday, 5, 0);
+					newRoot.add(sunday, 6, 0);
+
+					newRoot.setPadding(new Insets(10));
+					newRoot.setVgap(15);
+					newRoot.setHgap(15);
+					Stage stage = new Stage();
+					stage.setTitle("New Visualized Schedule!!!!!");
+					stage.setScene(new Scene(newRoot, 750, 200));
+					stage.show();
+				}
+			});
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	private class Course {
 	
+		private String courseCode;
+		private String professor;
+		private String weekdays1;
+		private String time1;
+		private String weekdays2;
+		private String time2;
+
+		public Course(String[] course) {
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
