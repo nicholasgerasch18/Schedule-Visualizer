9f80ee398fab3d67be8609803424cefc09d8ecb3
justincassens18 <justincassens18@OlinLab204-03.augustana.edu>
2021-10-28 11:35:43 -0500

    this sucks

--- Schedule Visualizer/src/application/Main.java
+++ Schedule Visualizer/src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
-			System.out.print("hi");
+			System.out.print("hello there");
 			primaryStage.setTitle("Schedule Visualizer");
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root, 400, 400);
