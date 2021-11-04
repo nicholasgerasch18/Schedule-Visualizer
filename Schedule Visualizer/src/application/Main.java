ab65c89a3d4e3273f23abd8ce3782bbcef145f7b
gabrielvallejo19 <gabrielvallejo19@OlinLab204-07.augustana.edu>
2021-11-04 10:14:34 -0500

    Added Old Main

--- Schedule Visualizer/src/application/Main.java
+++ Schedule Visualizer/src/application/Main.java
@@ -26,7 +26,7 @@ public class Main extends Application {
 			"MUCH", "MUEN", "MUSC", "PHIL", "PHYS", "POLS", "PSYC", "PUBH", "RELG", "SCAN", "SLP", "SOAN", "SPAN",
 			"SPST", "SWED", "THEA", "WGSS"));
 	final ArrayList<String> BUILDING_NAMES = new ArrayList<String>(
-			Arrays.asList("BERG", "CARV", "EVLD", "LIND", "OLIN", "SCIE", "SORN"));
+			Arrays.asList("BERG", "CARV", "EVLD", "LIND", "OLIN", "SCIE", "SORN", "OLDM"));
 	final ArrayList<String> DAYS = new ArrayList<String>(Arrays.asList("M ", "Tu ", "W ", "Th ", "F ", "M W F ",
 			"Tu Th ", "M W ", "M F ", "W F ", "Tu W Th F ", "M Tu W Th F "));
 
